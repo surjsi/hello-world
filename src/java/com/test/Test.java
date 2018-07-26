d142d0091aec11b04137090a15173e726b6fa8ed
Sing <surjit.sing.infotechlimited@sonymusic.com>
2018-07-26 19:39:44 -0400

    jenkins

---
+++ src/java/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public Class Test{
+	
+	public static void main(String[] args){
+		System.out.println("Test me - Main");
+	}
+	
+	public void (){
+		System.out.println("Test me");
+	}
+}
